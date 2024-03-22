3053c1f29e78e10fdda7bb8d27165649423d2c47
TuKaAri <123382819+FlorianHoy@users.noreply.github.com>
2024-03-22 16:40:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Welcome to the Flask CI/CD Demo! This is V2! "
+  return "Welcome to the Flask CI/CD Demo! This is V3! "
 
 
 @app.route("/new-deployment")
